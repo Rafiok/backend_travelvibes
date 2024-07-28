167faa621306c3bc65b6d34fcb45a78480e2378a
georis <georiseno@gmail.com>
2024-07-28 08:20:06 +0100

    Change asgi

--- voyageensemble/asgi.py
+++ voyageensemble/asgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perfectlov.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voyageensemble.settings')
 django.setup()
 
 from app import routing
